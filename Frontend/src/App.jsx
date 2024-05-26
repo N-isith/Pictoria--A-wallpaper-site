275f0b5f1c40be9044c6dda477bda52a2dee4420
N-isith <nisithalakshan94@gmail.com>
2024-05-26 14:00:08 +0530

    homepage configured

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <div className='App'>
-        <h2>ni</h2>
-      </div>
-
-    </>
+    <Router>
+      <>
+        <div className='App flex flex-col min-h-screen'>
+          <Routes>
+            <Route path='/' element={<Navbar />}/>
+          </Routes>
+        </div>
+      </>
+    </Router>
   )
 }
 
---
+++ Frontend/src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function Homepage() {
+  return (
+    <div className='thediv flex'>
+        <Navbar/>
+        <Homepage />
+        <Footer />
+    </div>
+  )
+}
