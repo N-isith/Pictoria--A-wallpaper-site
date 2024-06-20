39b6043a8afa15a03c9957b8625293a71504acd6
Nizith <nisithalakshan94@gmail.com>
2024-06-20 22:04:30 +0530

    imager signup backend completed

--- Backend/controller/imagerController.js
+++ Backend/controller/imagerController.js
@@ -1,23 +1,28 @@
-const Imager = require('../models/imagerModel.js');
+const imagerCreate = require('../models/imagerModel.js');
 
 const createImager = (async(req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const { username, password } = req.body;
 
-    const newImager = new Imager({
-        username,
-        password,
-    });
+    try {
+        const existUser = await imagerCreate.findOne({ username });
 
-    newImager.save()
-    .then( () => {
-        res.json("Imager Created.");
-    })
-    .catch( (err) => {
-        res.json("Imager creation failed", err);
-        console.log(err);
-    })
-    
+        if (existUser) {
+            console.log(Error)
+            return res.status(400).json({ message: 'Email already exists.' });
+        }
+        const newImager = new imagerCreate({
+            username,
+            password
+        });
+        await newImager.save();
+
+        res.status(201).json({ message: 'Imager created successfully', user: newImager });
+
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Imager creation failed', error });
+    }
 });
 
 module.exports = {createImager};
--- Backend/models/imagerModel.js
+++ Backend/models/imagerModel.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+const yup = require("yup");
 
 const schema = mongoose.Schema;
 
-const imagerSchema = new schema( {
+const ImagerValidateSchema = yup.object().shape({
+    username: yup.string().email().required(),
+    password: yup.string().min(8, "Password should have atleast 8 characters").max(40).required()
+})
+
+const ImagerSchema = new schema( {
     username: {
         type: String,
         required: true,
@@ -13,6 +19,16 @@ const imagerSchema = new schema( {
     },
 })
 
-const imagerModel = mongoose.model("imager", imagerSchema);
+ImagerSchema.pre('save', async function(next){
+    try {
+        await ImagerValidateSchema.validate(this.toObject());
+        next();
+    }
+    catch (error) {
+        next(error);        
+    }
+})
+
+const imagerModel = mongoose.model("imager", ImagerSchema);
 
 module.exports = imagerModel;
